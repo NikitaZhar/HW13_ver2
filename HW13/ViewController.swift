af473b6a428af102066d2f5f2080727038ecf38a
Nikita Zharinov <nikita.zharinov@gmail.com>
2021-03-14 13:08:16 +0100

    Initial Commit

---
+++ HW13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW13
+//
+//  Created by Nikita Zharinov on 14/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
